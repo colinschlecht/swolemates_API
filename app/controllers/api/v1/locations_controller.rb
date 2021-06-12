d7a745e229e0ddb3919245d8b51648421e7194af
Erik Huang <ekhu94@gmail.com>
2021-06-12 11:02:02 -0700

    resolve location controller conflicts

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -2,11 +2,7 @@ class Api::V1::LocationsController < ApplicationController
     
     def index
         locations = Location.all
-<<<<<<< HEAD
         render json: locations
-=======
-        render json:diets
->>>>>>> 7708a6437b5fc7f4e002dc0fe85346b52b85eaf8
     end
 
     def show
@@ -16,16 +12,11 @@ class Api::V1::LocationsController < ApplicationController
 
     def create
         location = Location.create(location_params)
-<<<<<<< HEAD
         render json: location
-=======
-        render json: diet
->>>>>>> 7708a6437b5fc7f4e002dc0fe85346b52b85eaf8
     end
 
     def update
         location = Location.find(params[:id])
-<<<<<<< HEAD
             render json: location
         else 
             render json: {message: 'error!!'}
@@ -41,24 +32,5 @@ class Api::V1::LocationsController < ApplicationController
     private 
     def location_params
         params.require(:location).permit(:id, :latitude, :longitude)
-=======
-        if location.update(location_params)
-            render json: location
-        else
-            render json: {message: 'Error'}
-        end
-    end
-
-    def destroy
-        location = Location.find(params[:id])
-        location.destroy
-        render json: { message: 'Location Entry Deleted'}
-    end
-
-    private
-
-    def location_params
-        params.require(:location).permit(:latitude, :longitude)
->>>>>>> 7708a6437b5fc7f4e002dc0fe85346b52b85eaf8
     end
 end

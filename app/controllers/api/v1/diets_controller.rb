e8150254593eaeb7101de0197dba91a73a2b3284
oopanpan <oopanpan@gmail.com>
2021-06-12 10:06:10 -0700

    Diet controller show, create

--- app/controllers/api/v1/diets_controller.rb
+++ app/controllers/api/v1/diets_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::DietsController < ApplicationController
+
+    def show
+        diet = Diet.find(params[:id])
+        render json: diet
+    end
+
+    def create
+        diet = Diet.create(diet_params)
+        render json: diet
+    end
+
+    def update
+    end
 end

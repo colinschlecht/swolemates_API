e4a9bb53b6cc418f0834213b4dcc9fcdb2594c21
oopanpan <oopanpan@gmail.com>
2021-06-12 12:04:03 -0700

    match request relationship built, matched WIP

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,14 @@ class User < ApplicationRecord
   belongs_to :location
   belongs_to :music_preference
 
+  #? match request relationships
+  has_many :match_requests_as_requestor, class_name: :MatchRequest
+  has_many :match_requests_as_receiver, foreign_key: :friend_id, class_name: :MatchRequest
+
+  #? matched friend relationships
+  has_many :matches, ->(user){ where('user_id = ? OR friend_id = ?', user.id, user.id)}
+  has_many :friends, through: :matches
+
   has_secure_password
   validates :email, presence: true
 end

b69cdc50ee4b67bd78b30427116e71f6cf8f85da
Colin Schlecht <schlechtcolin@yahoo.com>
2021-06-12 20:06:57 -0700

    location update method updated

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,36 +1,33 @@
 class Api::V1::LocationsController < ApplicationController
-    
-    def index
-        locations = Location.all
-        render json: locations
-    end
+  def index
+    locations = Location.all
+    render json: locations
+  end
 
-    def show
-        location = Location.find(params[:id])
-        render json: location
-    end
+  def show
+    location = Location.find(params[:id])
+    render json: location
+  end
 
-    def create
-        location = Location.create(location_params)
-        render json: location
-    end
+  def create
+    location = Location.create(location_params)
+    render json: location
+  end
 
-    def update
-        location = Location.find(params[:id])
-            render json: location
-        else 
-            render json: {message: 'error!!'}
-        end
-    end
+  def update
+    location = Location.find(params[:id])
+    location.update(location_params)
+  end
 
-    def destroy 
-        location = Location.find(params[:id])
-        location.destroy
-        render json: {message: 'Location Deleted'}
-    end
-    
-    private 
-    def location_params
-        params.require(:location).permit(:id, :latitude, :longitude)
-    end
+  def destroy
+    location = Location.find(params[:id])
+    location.destroy
+    render json: { message: "Location Deleted" }
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:id, :latitude, :longitude)
+  end
 end
